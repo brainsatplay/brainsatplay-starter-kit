3448ddb48fe8136f8b347ce605761f6c6b03e4c7
Garrett <gflynn@usc.edu>
2022-07-13 10:23:14 -0700

    Removed nesting

---
+++ nodes/ratio/index.d.ts
@@ -0,0 +1,5 @@
+declare namespace _default {
+    const tag: string;
+    function operator(a: any, b?: number): number;
+}
+export default _default;
---
+++ nodes/sine/index.d.ts
@@ -0,0 +1,6 @@
+declare namespace _default {
+    export const tag: string;
+    export { sine as operator };
+}
+export default _default;
+declare function sine(time: any, frequency?: number, amplitude?: number, phase?: number, center?: number): number;
