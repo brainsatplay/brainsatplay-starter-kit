bae8684d5d3e990c40fd086e4067cf44c75d99c7
Garrett Michael Flynn <46533749+GarrettMFlynn@users.noreply.github.com>
2022-08-11 10:49:37 -0700

    Updated for latest version of WASL

--- plugins/focus/index.js
+++ plugins/focus/index.js
@@ -1,17 +1,11 @@
 
 
-import * as bandpowerPlugin from './bandpower/index.js';
-import * as fftPlugin from './fft/index.js';
-import * as ratioPlugin from './ratio/index.js';
+import bandpower from './bandpower/index.js';
+import fft from './fft/index.js';
+import ratio from './ratio/index.js';
 
-import pkg from './package.json' assert {type: 'json'};
-import graph from './index.wasl' assert {type: 'json'};
-
-export const bandpower = bandpowerPlugin
-export const fft = fftPlugin
-export const ratio = ratioPlugin
-
-export default {
-    package: pkg,
-    graph
+export {
+    bandpower,
+    fft,
+    ratio
 }
--- plugins/seconds/index.js
+++ plugins/seconds/index.js
@@ -1,7 +1,3 @@
 export const tag = "seconds"
-
-export const operator = () => Date.now()/1000
-
 export const loop = 1000/10 // 10 times a second
-
-export default operator
+export default () => Date.now()/1000
