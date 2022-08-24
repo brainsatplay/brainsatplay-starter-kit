c76bdf6f96a1c3d3cd43e7c7664aaf2663c71d6e
Garrett Michael Flynn <46533749+GarrettMFlynn@users.noreply.github.com>
2022-08-23 19:45:35 -0700

    Updated for new WASL

--- plugins/timeseries/index.js
+++ plugins/timeseries/index.js
@@ -1,24 +1,18 @@
 import 'https://cdn.jsdelivr.net/npm/visualscript@0.0.7/dist/index.esm.js'
 
 export let visualization;
-export const tagName= 'div'
+export const tagName= 'visualscript-timeseries-stream'
 
 export const style= {
     height: '100%',
     width: '100%'
 }
 
-export function onrender(self) {
-    visualization = document.createElement('visualscript-timeseries-stream') // don't use class declarations directly to avoid version overlap
-    self.appendChild(visualization)
-}
-
-
 
 export default function (data) {
-    if (visualization){
-        visualization.data = [data]
-        visualization.draw()
+    if (this.element){
+        this.element.data = [data]
+        this.element.draw()
     }
     return data
 }
