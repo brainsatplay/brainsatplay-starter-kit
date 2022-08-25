9b32b48d9a32c6aa719a7dd4a253b9505abdadf3
Garrett <gflynn@usc.edu>
2022-08-25 06:51:18 -0700

    Update index.js

--- plugins/timeseries/index.js
+++ plugins/timeseries/index.js
@@ -1,18 +1,15 @@
 import 'https://cdn.jsdelivr.net/npm/visualscript@0.0.7/dist/index.esm.js'
 
-export let visualization;
-export const tagName= 'visualscript-timeseries-stream'
+export const tagName = 'visualscript-timeseries-stream'
 
 export const style= {
     height: '100%',
     width: '100%'
 }
 
-
 export default function (data) {
     if (this.element){
         this.element.data = [data]
         this.element.draw()
     }
-    return data
 }
