0af1fd4ac5f7fc4e6df766c49be5ed0fbdc43b3d
Garrett <gflynn@usc.edu>
2022-09-02 17:01:57 -0700

    Update index.js

--- plugins/timeseries/index.js
+++ plugins/timeseries/index.js
@@ -1,14 +1,32 @@
 import 'https://cdn.jsdelivr.net/npm/visualscript@0.0.7/dist/index.esm.js'
 
-export const tagName = 'visualscript-timeseries-stream'
+export const tagName = 'div'
 export const style = {
     height: '100%',
     width: '100%'
 }
 
-export default function (dataToVisualize) {
-    if (this.element){
-        this.element.data = [dataToVisualize]
-        this.element.draw()
+export let stream;
+
+export function onrender(){
+    if (this.stream) this.stream.remove()
+    this.stream = document.createElement('visualscript-timeseries-stream')
+    this.element.appendChild(this.stream)
+}
+
+export let nChannels = 0
+export let channels = {}
+
+const arrayFactory = (data, length=1, position=0) => Array.from({length}, (_,i) => (position === i) ? data : [])
+export default function (data, time, tag) {
+    if (this.stream){
+        let transformed = arrayFactory(data, this.nChannels, this.channels[tag])
+        this.stream.data = transformed
+        if (this.channels[tag] === undefined) {
+           this.channels[tag] = this.nChannels // index
+           this.nChannels++ // increment
+           this.onrender() 
+        }
+        this.stream.draw()
     }
 }
